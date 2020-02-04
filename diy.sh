a38fd502bba1819b60989bc25221352b84f89db1
icyleaf <icyleaf.cn@gmail.com>
2020-02-04 16:38:06 +0800

    feat: 增加 openclash

--- diy.sh
+++ diy.sh
@@ -5,5 +5,13 @@
 # Author: P3TERX
 # Blog: https://p3terx.com
 #=================================================
+
 # Modify default IP
 sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
+
+# custom packages
+cd package/feeds
+
+svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash
+
+cd -
