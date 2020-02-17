0d18d6958db204de3569350b5c4a768926a2e999
icyleaf <icyleaf.cn@gmail.com>
2020-02-17 20:38:50 +0800

    feat: 增加从 github secret 设置 root 密码

--- diy.sh
+++ diy.sh
@@ -6,21 +6,38 @@
 # Blog: https://p3terx.com
 #=================================================
 
+##################################
+# Settings
+##################################
+echo "Configuring ..."
+# Modify default IP
+sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
+
+if [ -n $OPENWRT_ROOT_PASSWORD ]; then
+  echo "WARN: root password is changed from your secret, make sure you add 'OPENWRT_ROOT_PASSWORD' secret"
+  # Modify password of root if present (encoded password)
+  # For example: passwot = $1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.
+  sed -i "s/root::0:0:99999:7:::/root:${OPENWRT_ROOT_PASSWORD}:0:0:99999:7:::/g" package/base-files/files/etc/shadow
+fi
+
+# Modify default theme
+# sed -i 's/bootstrap/argon/g' openwrt/package/feeds/luci/luci-base/root/etc/config/luci
+
+##################################
+# Custom feed
+##################################
 # add lienol feed: such like passwall and themes
 echo "Add lienol packages feed"
 echo "src-git lienol https://github.com/Lienol/openwrt-package" >> feeds.conf.default
 ./scripts/feeds update -a
 ./scripts/feeds install -a
+echo "Reviewing feeds.conf.default"
+cat feeds.conf.default
 
-
-# Modify default IP
-sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
-
-# Modify default theme
-# sed -i 's/bootstrap/argon/g' openwrt/package/feeds/luci/luci-base/root/etc/config/luci
-
-# custom packages
-echo "Download custom packages"
+##################################
+# Custom package
+##################################
+echo "Download Custom packages"
 mkdir -p package/icyleaf
 cd package/icyleaf
 
