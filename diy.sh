d808955a77e6a31031b269cdce9f00b931409878
icyleaf <icyleaf.cn@gmail.com>
2020-02-17 23:10:43 +0800

    fix: 无法设置主题

--- diy.sh
+++ diy.sh
@@ -8,24 +8,6 @@
 OPENWRT_PATH=`pwd`
 
 ##################################
-# Settings
-##################################
-echo ""
-echo "Configuring ..."
-# Modify default IP
-sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
-
-if [ -n $OPENWRT_ROOT_PASSWORD ]; then
-  echo "WARN: root password is changed from your secret, make sure you add 'OPENWRT_ROOT_PASSWORD' secret"
-  # Modify password of root if present (encoded password)
-  # For example: passwot = $1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.
-  sed -i "s|root::0:0:99999:7:::|root:$OPENWRT_ROOT_PASSWORD:0:0:99999:7:::|g" package/base-files/files/etc/shadow
-fi
-
-# Modify default theme
-sed -i 's/bootstrap/argon/g' package/feeds/luci/luci-base/root/etc/config/luci
-
-##################################
 # Custom feed
 ##################################
 # add lienol feed: such like passwall and themes
@@ -50,3 +32,21 @@ git clone --depth=1 https://github.com/tty228/luci-app-serverchan.git
 svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash
 
 cd $OPENWRT_PATH
+
+##################################
+# Settings
+##################################
+echo ""
+echo "Configuring ..."
+# Modify default IP
+sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
+
+if [ -n $OPENWRT_ROOT_PASSWORD ]; then
+  echo "WARN: root password is changed from your secret, make sure you add 'OPENWRT_ROOT_PASSWORD' secret"
+  # Modify password of root if present (encoded password)
+  # For example: passwot = $1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.
+  sed -i "s|root::0:0:99999:7:::|root:$OPENWRT_ROOT_PASSWORD:0:0:99999:7:::|g" package/base-files/files/etc/shadow
+fi
+
+# Modify default theme
+sed -i 's/bootstrap/argon/g' package/feeds/luci/luci-base/root/etc/config/luci
